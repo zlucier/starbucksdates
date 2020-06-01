65b45a59cea2639766e51b413d16ca655168c363
zlucier <66230122+zlucier@users.noreply.github.com>
2020-05-31 21:36:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+function renderToday(){
+	//Greet
+	var currentTime = new Date();
+	var h = currentTime.getHours();
+	var greet;
+		if (h < 12)
+			greet = 'Good Morning ☀️';
+		else if (h >= 12 && h <= 17)
+			greet = 'Good Afternoon ☕️';
+		else if (h >= 17 && h <= 24)
+			greet = 'Good Evening 🌙';
+	document.getElementById("greeting").innerHTML = greet;
+	//Today
+	var myDate = new Date();
+	var day = myDate.getDay();
+	var month = myDate.getMonth();
+	var date = myDate.getDate();
+
+	const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+	var tdate = "Today is " + days[day] + ", " + months[month] + " " + date;
+	document.getElementById("today").innerHTML = tdate;
+	//x days from Today
+	function addToday(x){
+		var myDatex = myDate.addDays(x);
+		var dayx = myDatex.getDay();
+		var monthx = myDatex.getMonth();
+		var datex = myDatex.getDate();
+		var tdatex = days[dayx] + ", " + months[monthx] + " " + datex;
+		return tdatex;
+	}
+	document.getElementById("days2").innerHTML = addToday(2);
+	document.getElementById("days5").innerHTML = addToday(5);
+	document.getElementById("days7").innerHTML = addToday(7);
+	document.getElementById("days14").innerHTML = addToday(14);
+	document.getElementById("days30").innerHTML = addToday(30);
+}
+renderToday();
